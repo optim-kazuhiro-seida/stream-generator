32e531e63bcc639e51ec28632549541876a81262
optim-kazuhiro-seida <63087065+optim-kazuhiro-seida@users.noreply.github.com>
2020-10-07 17:15:14 +0900

    Update main.go

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 	} else if err := ioutil.WriteFile(filepath.Join(dir, "stream_"+strings.ToLower(*typeName)+".go"), src, 0644); err != nil {
 		log.Fatalf("Writing a file: %s", err)
 	} else {
-		log.Println("Generated")
+		log.Println("Generated "+ filepath.Join(dir, "stream_"+strings.ToLower(*typeName)+".go")
 	}
 }
 
